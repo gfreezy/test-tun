310e6ee3791cb62b6bf1367d5240f0495ad6d39e
gfreezy <gfreezy@gmail.com>
2019-12-02 19:00:42 +0800

    linux compatibility

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,13 @@ use sysconfig::setup_ip;
 fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    let mut socket = phy::TunSocket::new("utun");
+    let mut socket = phy::TunSocket::new("utun6");
     let name = socket.name();
-    setup_ip(&name, "192.168.10.1", "192.168.10.0/24");
+    if cfg!(target_os = "macos") {
+        setup_ip(name, "192.168.10.1", "192.168.10.0/24");
+    } else {
+        setup_ip(name, "192.168.10.1/24", "192.168.10.0/24");
+    }
     println!("utun name is {}", name);
     let mut buf = [0u8; 100];
     let _: std::io::Result<()> = task::block_on(async {
